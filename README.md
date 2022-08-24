a43f05d6cfff3eb56f6cfe3e9a600a426520c652
Colin <wfang@nd.edu>
2022-08-23 22:57:30 -0400

    Add instructions

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Polymath: Benford's Law and Fractal Geometry
-Calculating the coeﬀicients of the Rienmann mapping function for the exterior of the Mandelbrot set to the closed unit disk.
+## Introduction
+
+Calculating the coeﬀicients of the Riemann mapping function for the exterior of the Mandelbrot set to the closed unit disk.
+
+
+
+## Compute Coefficients
+
+To compute am and bm coefficients of the Laurent series of the Riemann mapping function, use **codes/coeff_compute.py**
+
+For example, to compute the first 7168 coefficients for both am and bm
+
+```
+$ python codes/coeff_compute.py --max_power 7168
+```
+
--- codes/coeff_compute.py
+++ codes/coeff_compute.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python
 
+'''Compute am and bm coefficients for the Laurent series.'''
+
 import sys
 from fractions import Fraction
+import argparse
 
 # d = 2
 class MandelbrotLaurentSeries:
@@ -17,13 +20,13 @@ class MandelbrotLaurentSeries:
             4: Fraction(1,8)
         }
     
-    def open_b(self, path):
+    def open_b(self, path): # Load b coefficients from file
         with open(path, "r") as f:
             lines = f.readlines()
             for l in lines:
                 k, num, denom = list(map(int, l.split(",")))
                 self.b_coeffs[k] = Fraction(num, denom)
-    def open_a(self, path):
+    def open_a(self, path): # Load a coefficients from file
         with open(path, "r") as f:
             lines = f.readlines()
             for l in lines:
@@ -55,31 +58,6 @@ class MandelbrotLaurentSeries:
         self.betas[(n,m)] = beta
         return beta
 
-    # # To Do: change to calculate alpha
-    # def get_alpha(self, n, m):
-    #     if (n,m) in self.alphas.keys():
-    #         return self.alphas[(n,m)]
-
-    #     if m == 0:
-    #         return Fraction(1)
-
-    #     # 2^(n+1)
-    #     u = 2 << n
-
-    #     if m <= u-2: 
-    #         return 0
-
-    #     summands = (self.get_alpha(n,k) * self.get_alpha(n, m-k)
-    #             for k in range(u-1, m-u+2)
-    #             )
-
-    #     alpha = (self.get_alpha(n+1,m) - sum(summands) - \
-    #             self.get_alpha(0, m-u+1)
-    #             )/2
-
-    #     self.alphas[(n,m)] = alpha
-    #     return alpha
-
     def get_b(self, m):
         if m in self.b_coeffs.keys():
             return self.b_coeffs[m]
@@ -92,7 +70,6 @@ class MandelbrotLaurentSeries:
         if m < 4:
             exit("m must be greater than 3")
         a = self.b_coeffs[m-2] * (-1) - sum(self.a_coeffs[j+1] * self.b_coeffs[m-j-1] for j in range(2, m-1))
-        #print(f"a({m}) = {self.b_coeffs[m-2] * (-1)} - {sum(self.a_coeffs[j+1] * self.b_coeffs[m-j-1] for j in range(2, m-1))}")
         return a
 
     def compute_bs(self, max_power):
@@ -128,27 +105,27 @@ class MandelbrotLaurentSeries:
 
 if __name__ == '__main__':
     ser = MandelbrotLaurentSeries()
-    ser.open_b("bm_coeffs_7168.csv")
-    ser.open_a("am_coeffs_6144_interm.csv")
-    max_power = 7168
 
-    compute_b = True
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_power', type=int, default=7168, help='maximum number of coefficients to compute')
+    args = parser.parse_args()
+    print("Total number of coefficients to compute:", args.max_power)
+    max_power = args.max_power # Set the maximum number of coefficients to compute
+    
+    compute_a = True
 
-    if compute_b:
-        #ser.compute_bs(max_power)
+    print("Computing b coefficients...")
+    ser.compute_bs(max_power)
+    with open(f"bm_coeffs_{max_power}.csv", 'w+') as f:
+        for i in range(max_power):
+            bi = ser.b_coeffs[i]
+            f.write(f"{i}, {bi.numerator}, {bi.denominator}\n")
+
+    print("Computing a coefficients...")
+    if compute_a:
         ser.compute_as(max_power)
-        # with open(f"bm_coeffs_{max_power}.csv", 'w+') as f:
-        #     for i in range(max_power):
-        #         bi = ser.b_coeffs[i]
-        #         f.write(f"{i}, {bi.numerator}, {bi.denominator}\n")
         with open(f"am_coeffs_{max_power}.csv", 'w+') as f:
             for i in range(max_power):
                 ai = ser.a_coeffs[i]
                 f.write(f"{i}, {ai.numerator}, {ai.denominator}\n")
-    # else:
-    #     ser.compute_as(max_power)
-    #     with open(f"am_coeffs_{max_power}.txt", 'w+') as f:
-    #         for i in range(max_power):
-    #             ai = ser.a_coeffs[i]
-    #             f.write(f"{i}, {ai.numerator}, {ai.denominator}\n")
 
